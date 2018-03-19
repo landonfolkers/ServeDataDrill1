4eb7c6a18fad4f3bdef8c1f73b3935e4d422d6e2
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-03-19 13:52:22 -0600

    added data file

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 let express = require('express')
 let cors = require('cors')
-// let data = require('./cohorts')
+let data = require('./data/cohorts')
 let app = express()
-
-let data = [{
-    id: 1,
-    cohortName: "17-01-WD-DP",
-    cohortCode: "g100",
-    numberOfStudents: 28
-},{
-    id: 2,
-    cohortName: "17-01-DS-GT",
-    cohortCode: "g105",
-    numberOfStudents: 24
-},{
-    id: 3,
-    cohortName: "17-02-WD-PX",
-    cohortCode: "g109",
-    numberOfStudents: 30
-},{
-    id: 4,
-    cohortName: "17-03-WD-BD",
-    cohortCode: "g110",
-    numberOfStudents: 29
-}];
 
 app.use(cors())
 
